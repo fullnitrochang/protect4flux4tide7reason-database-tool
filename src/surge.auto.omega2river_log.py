151b636646a8c403507136da6c4b7ba5971dbfa4
fullnitrochang <fullnitrochang@gmail.com>
2025-04-16 23:51:08 +0900

    Update development log at 2025-04-16 23:51:07

--- src/surge.auto.omega2river_log.py
+++ src/surge.auto.omega2river_log.py
@@ -216,3 +216,12 @@ class LinkedList:
         while current.next:
             current = current.next
         current.next = new_node
+
+# Update at 2025-04-16 23:51:07
+# Refactored the code
+def count_words(text):
+    words = text.split()
+    word_count = {}
+    for word in words:
+        word_count[word] = word_count.get(word, 0) + 1
+    return word_count
