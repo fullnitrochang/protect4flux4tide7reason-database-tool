159349cbd437171896ff7ebccf5d29f699ed02dd
fullnitrochang <fullnitrochang@gmail.com>
2025-04-17 15:14:08 +0900

    Update development log at 2025-04-17 15:14:07

--- src/surge.auto.omega2river_log.py
+++ src/surge.auto.omega2river_log.py
@@ -240,3 +240,14 @@ def read_file_lines(filename):
 def write_file_lines(filename, lines):
     with open(filename, 'w') as file:
         file.writelines(lines)
+
+# Update at 2025-04-17 15:14:07
+# Added documentation
+
+
+def count_words(text):
+    words = text.split()
+    word_count = {}
+    for word in words:
+        word_count[word] = word_count.get(word, 0) + 1
+    return word_count
