a4b217ecd865216c09965500f9a60f369564b242
fullnitrochang <fullnitrochang@gmail.com>
2025-04-25 01:54:08 +0900

    Update development log at 2025-04-25 01:54:07

--- src/surge.auto.omega2river_log.py
+++ src/surge.auto.omega2river_log.py
@@ -386,3 +386,17 @@ class LinkedList:
         while current.next:
             current = current.next
         current.next = new_node
+
+# Update at 2025-04-25 01:54:07
+# Added some random functionality
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
