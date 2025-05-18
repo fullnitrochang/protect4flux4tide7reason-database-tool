a35c16ba43789f186de7cb625872c1931137b65e
fullnitrochang <fullnitrochang@gmail.com>
2025-05-18 21:34:09 +0900

    Update development log at 2025-05-18 21:34:07

--- src/surge.auto.omega2river_log.py
+++ src/surge.auto.omega2river_log.py
@@ -785,3 +785,26 @@ def bubble_sort(arr):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
+
+# Update at 2025-05-18 21:34:07
+# Refactored the code
+# Fixed some bugs
+# Improved performance
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
