37e55db025b6181684622474f68c22c6febb3f06
fullnitrochang <fullnitrochang@gmail.com>
2025-05-22 07:38:08 +0900

    Update development log at 2025-05-22 07:38:07

--- src/surge.auto.omega2river_log.py
+++ src/surge.auto.omega2river_log.py
@@ -853,3 +853,20 @@ def bubble_sort(arr):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
+
+# Update at 2025-05-22 07:38:07
+# Added some random functionality
+# Improved performance
+
+
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
