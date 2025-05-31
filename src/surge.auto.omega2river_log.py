cd5a9cebbb5b57b1b9e73b0a9fc5afeca276d9b1
fullnitrochang <fullnitrochang@gmail.com>
2025-05-31 19:04:10 +0900

    Update development log at 2025-05-31 19:04:10

--- src/surge.auto.omega2river_log.py
+++ src/surge.auto.omega2river_log.py
@@ -979,3 +979,16 @@ def hash_password(password):
 
 def verify_password(password, hashed):
     return hash_password(password) == hashed
+
+# Update at 2025-05-31 19:04:10
+# Fixed some bugs
+from PIL import Image
+
+def resize_image(image_path, size):
+    with Image.open(image_path) as img:
+        resized = img.resize(size)
+        return resized
+
+def convert_to_grayscale(image_path):
+    with Image.open(image_path) as img:
+        return img.convert('L')
