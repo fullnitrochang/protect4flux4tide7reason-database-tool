1a655ba282c16a84233e4a91d621f00af7c15411
fullnitrochang <fullnitrochang@gmail.com>
2025-10-11 02:35:12 +0900

    Update development log at 2025-10-11 02:35:10

--- src/surge.auto.omega2river_log.py
+++ src/surge.auto.omega2river_log.py
@@ -1411,3 +1411,26 @@ def create_table(conn):
         conn.commit()
     except sqlite3.Error as e:
         print(e)
+
+# Update at 2025-10-11 02:35:10
+# Added unit tests
+# Optimized the algorithm
+# Refactored the code
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
